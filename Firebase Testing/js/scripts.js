6d01e2e36c718a0381842b48b1ad42b534f804bd
AwkwardMongoose <82236528+AwkwardMongoose@users.noreply.github.com>
2022-04-20 14:24:32 -0400

    .

--- Firebase Testing/js/scripts.js
+++ Firebase Testing/js/scripts.js
@@ -48,11 +48,11 @@ onValue(starCountRef, (snapshot) => {
 });
 
 
-var input = document.getElementById('input');
+var input = document.getElementById('input').innerHTML;
 const button = document.getElementById('button');
 button.addEventListener('click', function() {
     let name = toString(input);
     console.log(toString(input))
-    console.log('Test 0.1')
+    console.log('Test 0.2')
     writeUserData('1', name)
 })
